f1ca165082b40ec17ac6912c0a6f1d108313a061
studorie <108615497+studorie@users.noreply.github.com>
2024-08-19 16:40:51 +0100

    Add files via upload

---
+++ Basic JavaScript Projects/Project1_expressions_alert/js/main.js
@@ -0,0 +1,29 @@
+// Display an alert with the message "Hello, world!"
+alert("Hello, world!");
+
+// Display the same message using the window object
+window.alert("Hello, world!");
+
+// Write "Hello, world!" to the HTML document
+document.write("Hello, world!");
+
+// Define a variable A with a string value and write it to the HTML document
+var A = "<br>This is for the variable assignment";
+document.write(A);
+
+// Define a variable B with a string value and display it in an alert
+var B = "This is the alert for the variable assignment";
+window.alert(B);
+
+// Write a string with escaped characters to the HTML document
+document.write("<br>He said, \"It\'s a beautiful day!\"");
+
+// Define two variables with string values
+var Sent1 = "This is the beginning of the string";
+var Sent2 = " and this is the end of the string";
+
+// Concatenate Sent1 and Sent2 and write the result to the HTML document
+document.write("<br>" + Sent1 + Sent2);
+
+// Display the concatenated string in an alert cause why not
+window.alert(Sent1 + Sent2);
